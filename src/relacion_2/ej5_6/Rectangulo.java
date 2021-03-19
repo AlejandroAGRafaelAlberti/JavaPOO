35667aa264df3a19ce2b8398f920f923c2e5dbe5
Aletuno <aaceytuno.alumno@iesrafaelalberti.es>
2021-03-19 19:02:11 +0100

    Rectangulo error fix

--- src/relacion_2/ej5_6/Rectangulo.java
+++ src/relacion_2/ej5_6/Rectangulo.java
@@ -3,19 +3,20 @@ package relacion_2.ej5_6;
     public class Rectangulo{
         private int altura;
         private int base;
-        private int perimetro;
-        private int area;
 
         public int getAltura() {
             return altura;
         }
+        public int getBase() {
+            return base;
+        }
 
         public int getPerimetro() {
-            return perimetro;
+            return altura*2 + base*2;
         }
 
         public int getArea() {
-            return area;
+            return altura*base;
         }
 
         @Override
@@ -23,42 +24,24 @@ package relacion_2.ej5_6;
             return "Rectangulo{" +
                     "altura=" + altura +
                     ", base=" + base +
-                    ", perimetro=" + perimetro +
-                    ", area=" + area +
                     '}';
         }
 
-        public void setAltura(int altura) {
-            if (!(altura==0)){
-                this.altura = altura;
-                if(altura<0){
-                    this.altura = -altura;
-                }
-                this.perimetro = altura*2 + base*2;
-                this.area = altura*base;
+        public void setAltura(int altura){
+            this.altura = altura;
+            if(altura<0){
+                this.altura = -altura;
             }
-
         }
-
         public Rectangulo(int altura, int base) {
             this.altura = altura;
             this.base = base;
-            this.perimetro = altura*2 + base*2;
-            this.area = altura*base;
-        }
-
-        public int getBase() {
-            return base;
         }
 
         public void setBase(int base) {
-            if (!(base==0)){
-                this.base = base;
-                if(base<0){
-                    this.base = -base;
-                }
-                this.perimetro = altura*2 + base*2;
-                this.area = altura*base;
+            this.base = base;
+            if(base<0){
+                this.base = -base;
             }
         }
     }
